f08eca66de45ac9289fa744892e0204456ac3177
dohee121 <dohee121@naver.com>
2020-10-13 19:49:44 +0900

    검색창, 날짜 선택, 시간 선택, 예약하기 버튼

--- app/src/main/java/com/example/aparking/Map.java
+++ app/src/main/java/com/example/aparking/Map.java
@@ -1,7 +1,17 @@
 package com.example.aparking;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -9,6 +19,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Calendar;
 
 
 public class Map extends AppCompatActivity
@@ -25,6 +36,9 @@ public class Map extends AppCompatActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        btnSelectDate = (Button)findViewById(R.id.btnDate);
+        btnSelectTime= (Button)findViewById(R.id.btnTime);
     }
 
     @Override
@@ -51,4 +65,46 @@ public class Map extends AppCompatActivity
 
     }
 
+    public void reserve(View v){
+        Toast.makeText(this.getApplicationContext(), "예약하기 클릭", Toast.LENGTH_LONG).show();
+    }
+
+    Button btnSelectDate, btnSelectTime;
+    class DateSetListener implements DatePickerDialog.OnDateSetListener {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth){
+            btnSelectDate.setText(dayOfMonth + "/" + (month+1) + "/" + year);
+        }
+    }
+    DateSetListener dateSetListener = new DateSetListener();
+
+    class TimeSetListener implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            btnSelectTime.setText(hourOfDay + ":" + minute);
+        }
+    }
+    TimeSetListener timeSetListener = new TimeSetListener();
+
+
+    public void onClick(View v) {
+        Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+        int mHour = c.get(Calendar.HOUR_OF_DAY);
+        int mMinute = c.get(Calendar.MINUTE);
+
+        if(v == btnSelectDate) {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    dateSetListener,  mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+        if(v == btnSelectTime) {
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    timeSetListener, mHour, mMinute, false);
+            timePickerDialog.show();
+        }
+    }
 }
