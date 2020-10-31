41f2561f9fab7dfe08e8396d55bbed1c78147ba1
HyeWon <qorgp13@naver.com>
2020-11-01 03:56:03 +0900

    nothing

--- app/src/main/java/com/example/aparking/ui/Check_qr.java
+++ app/src/main/java/com/example/aparking/ui/Check_qr.java
@@ -1,43 +1,45 @@
-package com.example.aparking;
+package com.example.aparking.ui;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
+import com.example.aparking.R;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
-public class Check_qr extends AppCompatActivity {
+public class Check_qr extends Fragment {
     private Button createQRbtn;
     private Button scanQRbtn;
-    private ImageView iv;
+
     private String text;
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_check_qr);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        final View root = inflater.inflate(R.layout.activity_check_qr, container, false);
 
-        Intent it = getIntent();
+        //Intent it = getIntent();
 
-        createQRbtn = (Button)findViewById(R.id.nav_btn);
-        scanQRbtn = findViewById(R.id.call_btn);
+        createQRbtn = (Button)root.findViewById(R.id.nav_btn);
+        scanQRbtn = root.findViewById(R.id.call_btn);
 
         createQRbtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 //Intent intent = new Intent(MainActivity.this, CreateQR.class);
                 //startActivity(intent);
 
-                iv = findViewById(R.id.qrcode);
+                ImageView iv = root.findViewById(R.id.qrcode);
                 text = "123";
 
                 MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
@@ -54,10 +56,11 @@ public class Check_qr extends AppCompatActivity {
         scanQRbtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 //Toast.makeText(getApplicationContext(),"확인 누름",Toast.LENGTH_LONG).show();
-              //  Intent intent = new Intent(MainActivity.this, ScanQR.class);
+                //  Intent intent = new Intent(MainActivity.this, ScanQR.class);
                 //startActivity(intent);
             }
         });
-    }
 
+        return root;
+    }
 }
--- app/src/main/java/com/example/aparking/ui/consultant/ConsultantFragment.java
+++ app/src/main/java/com/example/aparking/ui/consultant/ConsultantFragment.java
@@ -1,9 +1,11 @@
 package com.example.aparking.ui.consultant;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,9 +15,10 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.aparking.R;
+import com.example.aparking.ui.Check_qr;
 
 public class ConsultantFragment extends Fragment {
-
+    Button btn;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +26,14 @@ public class ConsultantFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_consultant, container, false);
         final TextView textView = root.findViewById(R.id.text_consultant);
         textView.setText("Hi");
+
+        btn=root.findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         return root;
     }
 }
