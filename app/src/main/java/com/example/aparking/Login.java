7cc788caa21098be621d82986745e8069adeef91
OHJUYEONG <ojy4475@naver.com>
2020-11-01 20:24:12 +0900

    login -sign in

--- app/src/main/java/com/example/aparking/Login.java
+++ app/src/main/java/com/example/aparking/Login.java
@@ -2,7 +2,10 @@ package com.example.aparking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Button b = (Button) findViewById(R.id.gotoPasswordResetButton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        Signin.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+
+            }
+        });
     }
 }
