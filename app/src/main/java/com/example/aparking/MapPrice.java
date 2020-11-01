3b3501bb2fe4960c9f2c0987a4fc89cc2f450f28
OHJUYEONG <ojy4475@naver.com>
2020-11-01 19:56:39 +0900

    sign in sns sign in connect

--- app/src/main/java/com/example/aparking/MapPrice.java
+++ app/src/main/java/com/example/aparking/MapPrice.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.aparking.ui.Check_qr;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
--- app/src/main/java/com/example/aparking/Signin.java
+++ app/src/main/java/com/example/aparking/Signin.java
@@ -17,6 +17,16 @@ public class Signin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
+        Button b = (Button) findViewById(R.id.button2);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        SnsSignin.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
 
+            }
+        });
     }
 }
