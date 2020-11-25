1fddf80b3aa45dcc9e65ef944740e6917a5ba662
HyeWon <qorgp13@naver.com>
2020-11-25 22:40:55 +0900

    chat fragment change, design modify

--- app/src/main/java/com/example/aparking/ui/consultant/ConsultantFragment.java
+++ app/src/main/java/com/example/aparking/ui/consultant/ConsultantFragment.java
@@ -1,11 +1,22 @@
 package com.example.aparking.ui.consultant;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.DataSetObserver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,25 +26,123 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.aparking.R;
-import com.example.aparking.ui.Check_qr;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ConsultantFragment extends Fragment {
-    Button btn;
+    ListView listView;
+    private EditText chatText;
+    ChatArrayAdapter chatArrayAdapter;
+    private boolean side = false;
+    private Button buttonSend;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_consultant, container, false);
-        final TextView textView = root.findViewById(R.id.text_consultant);
-        textView.setText("Hi");
+        View root = inflater.inflate(R.layout.activity_chat, container, false);
 
-        btn=root.findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        buttonSend = (Button) root.findViewById(R.id.buttonSend);
 
+        listView = (ListView) root.findViewById(R.id.listview);
+
+        chatArrayAdapter = new ChatArrayAdapter(inflater.getContext(), R.layout.activity_chat_singlemessage);
+        listView.setAdapter(chatArrayAdapter);
+
+        chatText = (EditText) root.findViewById(R.id.chatText);
+        chatText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    return sendChatMessage();
+                }
+                return false;
             }
         });
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                sendChatMessage();
+            }
+        });
+
+        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        listView.setAdapter(chatArrayAdapter);
+
+        chatArrayAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(chatArrayAdapter.getCount() - 1);
+            }
+        });
+
         return root;
+    }
+
+    private boolean sendChatMessage(){
+        chatArrayAdapter.add(new ChatMessage(side, chatText.getText().toString()));
+        chatText.setText("");
+        side = !side;
+        return true;
+    }
+
+    public class ChatArrayAdapter extends ArrayAdapter {
+        Context context;
+        private TextView chatText;
+        private List chatMessageList = new ArrayList();
+        private LinearLayout singleMessageContainer;
+
+
+        public void add(ChatMessage object) {
+            super.add(object);
+            chatMessageList.add(object);
+        }
+
+
+        public ChatArrayAdapter(Context context, int textViewResourceId) {
+            super(context, textViewResourceId);
+            this.context = context;
+        }
+
+        public int getCount() {
+            return this.chatMessageList.size();
+        }
+
+        public ChatMessage getItem(int index) {
+            return (ChatMessage) this.chatMessageList.get(index);
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View row = convertView;
+
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.activity_chat_singlemessage, null, true);
+
+            singleMessageContainer = (LinearLayout) row.findViewById(R.id.singleMessageContainer);
+            ChatMessage chatMessageObj = getItem(position);
+            chatText = (TextView) row.findViewById(R.id.singleMessage);
+            chatText.setText(chatMessageObj.message);
+            chatText.setBackgroundResource(chatMessageObj.left ? R.drawable.bubble_a : R.drawable.bubble_b);
+            singleMessageContainer.setGravity(chatMessageObj.left ? Gravity.LEFT : Gravity.RIGHT);
+            return row;
+
+        }
+
+        public Bitmap decodeToBitmap(byte[] decodedByte) {
+            return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
+        }
+    }
+
+    public class ChatMessage {
+        public boolean left;
+        public String message;
+
+        public ChatMessage(boolean left, String message) {
+            super();
+            this.left = left;
+            this.message = message;
+        }
     }
 }
