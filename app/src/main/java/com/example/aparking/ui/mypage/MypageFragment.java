6594424c3f96f04ea35c241952c7e88d9a0252e9
dohee121 <dohee121@naver.com>
2020-11-28 16:36:45 +0900

    내 정보 클릭이벤트 추가

--- app/src/main/java/com/example/aparking/ui/mypage/MypageFragment.java
+++ app/src/main/java/com/example/aparking/ui/mypage/MypageFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,20 +18,32 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.aparking.R;
 
 public class MypageFragment extends Fragment {
+    LayoutInflater parent;
+    Button changeBtn;
+    LinearLayout addCardBtn;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
+        parent = inflater;
         View root = inflater.inflate(R.layout.activity_mypage, container, false);
 
+        /* 내 정보 변경 Click 처리 */
+        changeBtn = root.findViewById(R.id.changeBtn);
+        changeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(parent.getContext(), "내 정보 변경", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        /* 카드 등록 Click 처리 */
+        addCardBtn = root.findViewById(R.id.addCardBtn);
+        addCardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(parent.getContext(), "카드 등록하기", Toast.LENGTH_LONG).show();
+            }
+        });
         return root;
-    }
-
-    public void changeInfo(View v){
-        Toast.makeText(getActivity(), "내 정보 변경", Toast.LENGTH_LONG).show();
-    }
-
-    public void addCard(View v){
-        Toast.makeText(getActivity(), "카드 등록하기", Toast.LENGTH_LONG).show();
     }
 }
