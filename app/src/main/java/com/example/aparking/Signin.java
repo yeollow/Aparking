373363221ea483e37a773c4af1653d4e63d0114e
unknown <ljy2784437@gmail.com>
2020-12-03 02:59:30 +0900

    layout

--- app/src/main/java/com/example/aparking/Signin.java
+++ app/src/main/java/com/example/aparking/Signin.java
@@ -9,6 +9,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.StrictMode;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +20,8 @@ import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.regex.Pattern;
+
 public class Signin extends AppCompatActivity implements View.OnClickListener, DialogInterface.OnCancelListener {
     static final String mailSender = "ljy2784437@gmail.com";
     static final String mailSenderPW = "Dlwjdduf1!";
@@ -25,10 +29,14 @@ public class Signin extends AppCompatActivity implements View.OnClickListener, D
     TextInputLayout authEmail;
     TextInputLayout username;
     TextInputLayout passwd;
+    TextInputLayout confirmPw;
     TextInputLayout phone;
+
+    EditText authEmailText;
 
     String acc_name;
     String acc_pw;
+    String acc_pw_confirm;
     String phone_num;
     String acc_email;
 
@@ -87,37 +95,67 @@ public class Signin extends AppCompatActivity implements View.OnClickListener, D
         switch (v.getId()) {
             case R.id.authButton:
                 username = findViewById(R.id.username);
+                username.setCounterEnabled(true);
+
                 passwd = findViewById(R.id.passwd);
                 phone = findViewById(R.id.phone);
+
+                confirmPw = findViewById(R.id.cofirm_pw);
 
                 acc_name = username.getEditText().getText().toString();
                 acc_pw = passwd.getEditText().getText().toString();
                 phone_num = phone.getEditText().getText().toString();
                 acc_email = authEmail.getEditText().getText().toString();
+                acc_pw_confirm = confirmPw.getEditText().getText().toString();
 
-                dialog = LayoutInflater.from(this);
+                authEmailText = authEmail.getEditText();
+                authEmailText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        boolean email_check = Pattern.matches("\\w+@\\w+\\.\\w+(\\.\\w+)?", acc_email);
+
+                        if (!email_check)
+                            authEmailText.setError("올바른 이메일 형식을 사용해주세요");
+                        else
+                            authEmailText.setError(null);
+                    }
+                });
+
+                if (!acc_pw_confirm.equals(acc_pw)) {
+                    Toast.makeText(getApplicationContext(), "비밀번호를 확인해주세요", Toast.LENGTH_LONG).show();
+                } else {
+                    dialog = LayoutInflater.from(this);
 //                layout resource를 view의 형태로 반환
-                dialogLayout = dialog.inflate(R.layout.auth_dialog, null);
-                authDialog = new Dialog(this);
-                authDialog.setContentView(dialogLayout);        //Dialog에 inflate한 view를 삽입
-                authDialog.setCanceledOnTouchOutside(false);       //Dialog 바깥부분 선택 시 닫히지 않음.
-                authDialog.setOnCancelListener(this);
-                authDialog.show();      //show
+                    dialogLayout = dialog.inflate(R.layout.auth_dialog, null);
+                    authDialog = new Dialog(this);
+                    authDialog.setContentView(dialogLayout);        //Dialog에 inflate한 view를 삽입
+                    authDialog.setCanceledOnTouchOutside(false);       //Dialog 바깥부분 선택 시 닫히지 않음.
+                    authDialog.setOnCancelListener(this);
+                    authDialog.show();      //show
 
-                try {
-                    GMailSender GmailSender = new GMailSender(mailSender, mailSenderPW);
-                    SMTPEmailCode = GmailSender.getEmailCode();
-                    userEmailAddress = authEmail.getEditText().getText().toString();     //사용자 입력 email정보
+                    try {
+                        GMailSender GmailSender = new GMailSender(mailSender, mailSenderPW);
+                        SMTPEmailCode = GmailSender.getEmailCode();
+                        userEmailAddress = authEmail.getEditText().getText().toString();     //사용자 입력 email정보
 
-                    GmailSender.sendMail("Aparking 인증코드 입니다", SMTPEmailCode, userEmailAddress);
-                } catch (Exception e) {
-                    e.printStackTrace();
+                        GmailSender.sendMail("Aparking 인증코드 입니다", SMTPEmailCode, userEmailAddress);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    countdownTimer();
+
+
+                    break;
                 }
-
-                countdownTimer();
-
-
-                break;
         }
     }
 
@@ -155,6 +193,7 @@ public class Signin extends AppCompatActivity implements View.OnClickListener, D
 //                    회원정보 테이블은 aparking-d0735-2ba22 이거인듯.. 콘솔 들어가서 확인해봐ㅋㅋ 데이터베이스에 어떻게 값 넣는지도 한번 봐라.
 //                    분명 database 이름과 json객체 설정, json field(key,value)를 설정할 수 있는 방법이 있을거야
 //                    acc_name, acc_pw, phone_num, acc_email 전부 보내면 됨.
+
                     Toast.makeText(getApplicationContext(), "FirebaseDatabase로 acc_name, acc_pw, phone_num, acc_email을 보내고 저장하자. (회원가입 완료)", Toast.LENGTH_LONG).show();
 
                     authDialog.cancel();
