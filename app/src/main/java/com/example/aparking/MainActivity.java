3c47d9dd606ead3752c159606302ecb1bcb5b3c5
unknown <ljy2784437@gmail.com>
2020-12-05 03:51:48 +0900

    check_qr and home

--- app/src/main/java/com/example/aparking/MainActivity.java
+++ app/src/main/java/com/example/aparking/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.aparking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.os.StrictMode;
+
+import com.example.aparking.ui.Check_qr;
+import com.example.aparking.ui.home.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.contentLayout, HomeFragment.getInstance()).commit();
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.contentLayout, fragment).commit();
     }
 }
